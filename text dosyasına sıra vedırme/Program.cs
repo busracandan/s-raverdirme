a413b2ec9f053a48af770614dd7d159ebec1b609
busracandan <busra58cndn@hotmail.com>
2018-02-24 15:47:45 +0200

    ücüncü projem

---
+++ text dosyasına sıra vedırme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace text_dosyasına_sıra_vedırme
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
